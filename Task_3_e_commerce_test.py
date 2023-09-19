fa59684d5f941bb7106b63ea1de3a251c6755e24
Victor Owino <56676033+VictorOwinoKe@users.noreply.github.com>
2023-09-19 12:59:36 +0300

    Update Task_3_e_commerce_test.py

--- Task_3_e_commerce_test.py
+++ Task_3_e_commerce_test.py
@@ -8,11 +8,23 @@ def quicksort_products(products, ascending=True):
         return products
 
     pivot = products[len(products) // 2]
-    left = [product for product in products if (product.price < pivot.price) ascending else (product.price > pivot.price)]
+    left = []
+    right = []
+
+    for product in products:
+        if ascending:
+            if product.price < pivot.price:
+                left.append(product)
+            elif product.price > pivot.price:
+                right.append(product)
+        else:
+            if product.price > pivot.price:
+                left.append(product)
+            elif product.price < pivot.price:
+                right.append(product)
 
     middle = [product for product in products if product.price == pivot.price]
-    right = [product for product in products  if (product.price > pivot.price)  ascending else (product.price < pivot.price)]
-
+    
     return quicksort_products(left, ascending) + middle + quicksort_products(right, ascending)
 
 # Example usage:
